7c2970dc92b318f218359e2fad4ca9c0fac84d18
Mr.Jopa <pavelrum1998@mail.ru>
2022-01-31 19:04:53 +0500

    Array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] arr = { "qwe", "asdasd", "zxczxc", "nbv"};
+
+void PrintArray(string [] array)
+{
+    int count = array.Length;
+
+    for (int i = 0;  i < count ; i++)
+    {
+        Console.Write(n);
+        Console.WriteLine($"{array[i] }");
+    }
+    Console.WriteLine();
+}
+
+
+PrintArray(arr);
+
